4b5d4df52b2acc1e54462f5fc50a653adc2c0185
Thebot220 <theocosta2021@gmail.com>
2026-07-16 13:10:10 -0300

    Add files via upload

--- src/components/AdvantagesText.jsx
+++ src/components/AdvantagesText.jsx
@@ -1,7 +1,7 @@
 import Imagem from './Imagem.jsx'
 import styles from './AdvantagesText.module.css'
 
-function advantagesText({ alinhar = 'left', title, text , img, w, h, alt}) {
+function AdvantagesText({ alinhar = 'left', title, text , img, w, h, alt}) {
     const estiloTitulo = {
   textAlign: alinhar === 'right' ? 'right' : 'left',
   width: '100%',
@@ -9,18 +9,18 @@ function advantagesText({ alinhar = 'left', title, text , img, w, h, alt}) {
 };
 
     return (
-        <div className={styles.container}>
-        <div className={styles.Text}>
-            <h2 className={styles.title} style={estiloTitulo}>
-            {title}
-            </h2>
-            <p className={styles.text}>{text}</p>
-        </div>
-        <div className={styles.imagem}>
-            <Imagem src={img} width={w} height={h} alt={alt}/>
-        </div>
+        <div className={`${styles.container} ${alinhar === 'right' ? styles.inverseContainer : ''}`}>
+            <div className={styles.Text}>
+                <h2 className={styles.title} style={estiloTitulo}>
+                {title}
+                </h2>
+                <p className={styles.text}>{text}</p>
+            </div>
+            <div className={`${styles.imagem} ${alinhar === 'right' ? styles.inverseImagem : ''}`}>
+                <Imagem src={img} width={w} height={h} alt={alt}/>
+            </div>
         </div>
     )
 }
 
-export default advantagesText
+export default AdvantagesText
