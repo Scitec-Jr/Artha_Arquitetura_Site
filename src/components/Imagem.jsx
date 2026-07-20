6b2aa2a3c7dbcfe635fa59d47b877ace347820cb
Thebot220 <theocosta2021@gmail.com>
2026-07-20 17:34:52 -0300

    Delete src/components directory

--- src/components/Imagem.jsx
+++
@@ -1,6 +0,0 @@
-function Imagem({ src, alt, width, height }) {
-    return (
-        <img src={src} alt={alt} style={{width: width, height: height, objectFit: 'cover'}}/>
-    )
-}
-export default Imagem
